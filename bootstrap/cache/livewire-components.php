bb588f7341c9862012795bf56bc9619e17a7c6db
Hibatullah Dyfa Grahatama <82629289+dyfagrahatama@users.noreply.github.com>
2022-12-05 08:26:22 +0700

    Add files via upload

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'admin.user' => 'App\\Http\\Livewire\\Admin\\User',
+  'peminjam.kategori' => 'App\\Http\\Livewire\\Peminjam\\Kategori',
+  'peminjam.keranjang' => 'App\\Http\\Livewire\\Peminjam\\Keranjang',
+  'peminjam.motor' => 'App\\Http\\Livewire\\Peminjam\\Motor',
+  'petugas.chart' => 'App\\Http\\Livewire\\Petugas\\Chart',
+  'petugas.chart-script' => 'App\\Http\\Livewire\\Petugas\\ChartScript',
+  'petugas.kategori' => 'App\\Http\\Livewire\\Petugas\\Kategori',
+  'petugas.motor' => 'App\\Http\\Livewire\\Petugas\\Motor',
+  'petugas.penerbit' => 'App\\Http\\Livewire\\Petugas\\Penerbit',
+  'petugas.rak' => 'App\\Http\\Livewire\\Petugas\\Rak',
+  'petugas.transaksi' => 'App\\Http\\Livewire\\Petugas\\Transaksi',
+);
